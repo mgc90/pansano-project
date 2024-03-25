ba6d0bd8d01c92e62fbd89d3b8790ce5f323506f
Mati <matiascorrea@protonmail.com>
2024-03-25 19:00:27 -0300

    tutorial finalizado hasta minuto 40 que arrranca con react

--- api-backend-django/djangoCrudApi/settings.py
+++ api-backend-django/djangoCrudApi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'tasks'
 ]
 
@@ -128,3 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #cors authorization
 CORS_ALLOWED_ORIGINS = []
+
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
