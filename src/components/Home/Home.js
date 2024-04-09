85b81b3d49dc40253b94073578dc9aed513fa154
Mati <matiascorrea@protonmail.com>
2024-04-09 17:18:46 -0300

    añadiendo componentes de reactprime para completar dashboard. estilos modulados en products para evitar mezclas entre estilos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,14 @@
 import Navbar from "../Navbar/Navbar";
 import Banner from "../Banner/Banner";
 import Products from "../Products/Products";
+import styles from "../Products/Products.module.css"
 
 const Home = () => {
     return (
     <div>
         <Navbar />
         <Banner /> 
-        <div className="productCardContainer">
+        <div className={styles.productCardContainer}>
          <Products />
         </div>
         
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,7 +3,7 @@ import { dataContext } from "../Context/DataContext";
 import getAllProductos from "../../api/productos.api";
 
 
-import "./Products.css"
+import styles from "./Products.module.css"
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -12,6 +12,7 @@ const Products = () => {
 
   const { cart } = useContext(dataContext);
 
+  
 
   useEffect(() => {
     getAllProductos().then((res) => setData(res.data));
@@ -21,12 +22,12 @@ const Products = () => {
 
   return data.map((product) => {
     return (
-        <div className="card" key={product.id}>
-            <img src={product.img} alt="imgProductCard" className="imgCard" />
+        <div className={styles["product-card"]} key={product.id}>
+            <img src={product.img} alt="imgProductCard" className={styles.imgCard} />
             <h3 title="Nombre del producto">{product.name}</h3>
-            <h4 className="price" title="Precio en pesos argentinos">${product.price},00</h4>
+            <h4 className={styles.price} title="Precio en pesos argentinos">${product.price},00</h4>
             { cart.some((item) => item.id === product.id) ? 
-            (<button onClick={() => buyProducts(product)} className="yaAgregado" title="Producto ya agregado al carrito">
+            (<button onClick={() => buyProducts(product)} className={styles.yaAgregado} title="Producto ya agregado al carrito">
               En el Carrito({cart.map(item => item.id === product.id && item.quanty)})
               </button>) : 
             (<button onClick={() => buyProducts(product)}  title="Agregar producto al carrito" >Agregar al Carrito</button>)}
